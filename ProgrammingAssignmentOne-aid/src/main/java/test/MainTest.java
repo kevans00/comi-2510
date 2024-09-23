b5e4a5aad6c68a07b75d3eba7f8a1a0e8efa5574
Kendra <kendrabevans01@gmail.com>
2024-09-23 00:31:50 -0400

    [Main] Use correct PlanetaryDialog method for setting. Refactor readers
    Also removed some unneeded code. Moved the csv readers globally to
    anticipate their use by future methods

--- ProgrammingAssignmentOne-aid/src/main/java/test/MainTest.java
+++ ProgrammingAssignmentOne-aid/src/main/java/test/MainTest.java
@@ -1,49 +1,29 @@
 package test;
 
 import java.util.ArrayList;
-import javax.swing.JFrame; 
-import javax.swing.JComboBox;
+import java.lang.Math;
 
 import edu.ccri.assignment.planets.test.dialog.PlanetaryDialog;
 import planets.transportation.TransportationVehicle;
 import planets.transportation.TransportationVehicleReader;
 import planets.planetai.PlanetaryBody;
 import planets.planetai.PlanetaryBodyReader;
-import planets.util.DialogFrame;
 
 public class MainTest {
 	
+// ToDo: Make this get the CSV better
 public static final String PLANET_CSV = "C:\\\\Users\\\\Lenovo ThinkPad T430\\\\eclipse-workspace\\\\ProgrammingAssignmentOne-aid\\\\src\\\\main\\\\java\\\\planets\\\\planetai\\\\planet_details.csv";
 public static final String VEHICLE_CSV = "C:\\\\Users\\\\Lenovo ThinkPad T430\\\\eclipse-workspace\\\\ProgrammingAssignmentOne-aid\\\\src\\\\main\\\\java\\\\planets\\\\transportation\\\\vehicle_details.csv";
 public static final String DELIMITER = ",";
 
-	public static void makeFrame() {
-		JFrame frame = new JFrame(); // Create frame object
-		frame.setTitle("Planetary Travel Calculator"); // Set title of frame
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Exit out of application 'X'-out button; otherwise program will remain 'open' (hidden) when 'X' is pressed
-		frame.setResizable(false); // Prevent resizing of window (for now?)
-		frame.setSize(400,400); // Set size to be 200x200 pixels
 
-		// Add things into the frame; Invoked via new method for cleanliness ;)
-		// ToDo: Change this to grab the actual vehicle values
-		String[] vehicles = {"GR86", "Jetpack"};
-		
-		// Create a combo box (drop-down box element)
-		JComboBox jcb = new JComboBox(vehicles);
-		
-		frame.add(jcb); // add Combo box to the existing frame
-		frame.pack(); // Causes Window to be sized to fit the preferred size and layout of its subcomponents. Try commenting this out.
-		
-		// Make the frame object visible. 
-		// NOTE: setVisible must be run last. Otherwise anything items added to the box after this line 
-		// won't be visible because it 'technically' wasn't told to be visible later
-		frame.setVisible(true);  
-	}
+static PlanetaryBodyReader pb = new PlanetaryBodyReader();
+static ArrayList<PlanetaryBody> planetList = pb.readCSV(PLANET_CSV, DELIMITER);
+
+static TransportationVehicleReader tv = new TransportationVehicleReader();
+static ArrayList<TransportationVehicle> vehicleList = tv.readCSV(VEHICLE_CSV, DELIMITER);
 
 	public static String[] getPlanetData() {
-		PlanetaryBodyReader pb = new PlanetaryBodyReader();
-		ArrayList<PlanetaryBody> planetList = null;
-		planetList = pb.readCSV(PLANET_CSV, DELIMITER);
 		
 		int planetArraySize = planetList.size();
 		String planetArray[] = new String[planetArraySize];
@@ -55,11 +35,8 @@ public static final String DELIMITER = ",";
 		
 		return planetArray;
 	}
-		
+	
 	public static String[] getVehicleData() {
-		TransportationVehicleReader tv = new TransportationVehicleReader();
-		ArrayList<TransportationVehicle> vehicleList = null;
-		vehicleList = tv.readCSV(VEHICLE_CSV, DELIMITER);
 		
 		int vehicleArraySize = vehicleList.size();
 		String vehicleArray[] = new String[vehicleArraySize];
@@ -74,73 +51,30 @@ public static final String DELIMITER = ",";
 	
 	public static void main(String[] args) {
 
-		TransportationVehicleReader tvr = new TransportationVehicleReader();
-		PlanetaryBodyReader pbr = new PlanetaryBodyReader();
 		PlanetaryDialog pd = new PlanetaryDialog("Planetary Body Calculator");
-		
-		// Create Frame
-		//makeFrame();
-		///makeVehicleArray();
-		// Get Data
-		// Fill Frame with data
-		
-		// --------
-			//Planet getName
-			//Planet getDistanceFromSun & getDistanceFromEarth
-		//Ask user "Chose your starting planet"
-			//Store in startingPlanet
-		//Ask user "Chose your destination planet"
-			//Store in destinationPlanetOne
-		//Ask user "Chose your ship"
-			//Store in userShip - doesn't change
-		//Inform user of valid range values
-		//Calculate ship speed with drag
-		//Display time it takes to get to planet
-			//Store in time
-		//Repeat but start at destinationPlanetOne
-		//Ask user "Chose the next destination"
-			//Store in destinationPlanetTwo
-		//Calculate ship speed with drag
-		//Display time it takes to get to planet
-			//Store in timeTwo - time = time + timeTwo
-		//Repeat but start at destinationPlanetTwo
-		//Ask user "Chose the final destination"
-			//Store in destinationPlanetThree
-		//Calculate ship speed with drag
-		//Display total trip time
-			//Store in timeThree - time = time + timeThree
-		
-		ArrayList<TransportationVehicle> list = null;
-		list = tvr.readCSV(VEHICLE_CSV, DELIMITER);
-		int arraySize = list.size();
-		
-		String vehicleArray[] = new String[arraySize];
-		
-		for (int i=0; i < arraySize; i++) {
-			//System.out.printf("Vehicle at Index %s is %s", String.valueOf(i), vehicleArray[i].getVehicleName());
-			TransportationVehicle entry = list.get(i);
-			vehicleArray[i] = entry.getVehicleName();
-		}
 		
 		String[] planetData = getPlanetData();
 		String[] vehicleData = getVehicleData();
 		
-		//pd.setTransportationVehicleName("Toyota Corolla");
-		//pd.setStartingPlanetName("Earth");
-		//pd.setDestinationPlanetName("Saturn");
-		
-		pd.useTransporationVehicle();
-		pd.useStartingPlanet();
-		pd.useDestinationPlanet();
-		
+		pd.setUseTransporationVehicle();
+		pd.setUseStartingPlanet();
+		pd.setUseDestinationPlanet();
+
 		pd.showMultiEditDialog(planetData, vehicleData);
 		
-		/*
-		pd.getDestinationPlanetName();
-		pd.getTransportationVechicleName();
-		pd.getDestinationPlanetName();
-		pd.getStartingPlanetName();
-		*/
+		String chosenVehicle = pd.getTransportationVechicleName();
+		String startingPlanet = pd.getStartingPlanetName();
+		String endingPlanet = pd.getDestinationPlanetName();
+		
+		//New psuedocode:
+		//CalculateDistance();
+		//CalculateTripTime();
+		//Handle other misc calculations
+		//Get the results and format them
+		
+		// ToDo: make a message formatter to format all the results?
+		pd.showModalDialog("test");
+		
 	}
 }
 
