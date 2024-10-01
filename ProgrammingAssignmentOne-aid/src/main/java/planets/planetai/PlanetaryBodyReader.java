430648c844a0db059624d6ee41396a00851b0729
Kendra <kendrabevans01@gmail.com>
2024-10-01 02:36:16 -0400

    [Main] Make PlanetaryBodyReader assign classes during file read

--- ProgrammingAssignmentOne-aid/src/main/java/planets/planetai/PlanetaryBodyReader.java
+++ ProgrammingAssignmentOne-aid/src/main/java/planets/planetai/PlanetaryBodyReader.java
@@ -1,19 +1,25 @@
 package planets.planetai;
+import planets.planetai.PlanetaryBody;
+import planets.transportation.TransportationVehicleReader;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.LineNumberReader;
 import java.util.ArrayList;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+// import edu.ccri.assignment.planets.data.DataAccessOperations;
+
 
 /**
  * This class creates a PlanetaryBodyReader object that reads CSV files into an ArrayList
  */
 public class PlanetaryBodyReader {
-	
 	private static final Logger logger = LogManager.getLogger(PlanetaryBodyReader.class.getName());
-	
+		 
 	/**
 	 * Default Constructor
 	 */
@@ -28,51 +34,91 @@ public class PlanetaryBodyReader {
 	public ArrayList<PlanetaryBody> readCSV(String csvFile, String delimiter){
 		ArrayList<PlanetaryBody> list = new ArrayList<PlanetaryBody>();
 		PlanetaryBody detail = null;
-		BufferedReader br = null;
+		LineNumberReader ln = null;
 		
 		try {
 			File file = new File(csvFile);
 			FileReader fr = new FileReader(file);
 			
-			br = new BufferedReader(fr);
+			ln = new LineNumberReader(fr);
 			
 			String line = "";
 			String[] tempArr;
 			
-			line = br.readLine();
-			PlanetaryBodyReader.logger.debug("Header = " + line);
+			line = ln.readLine();
+			PlanetaryBodyReader.logger.debug("Header has line number " + ln.getLineNumber() + " has: " + line);
+			PlanetaryBodyReader.logger.debug("Beginning Object Classifications");
 			
-			while ((line = br.readLine()) != null) {
-				PlanetaryBodyReader.logger.debug("Line = " + line);	
+			while ((line = ln.readLine()) != null) {
+				PlanetaryBodyReader.logger.debug("Line number " + ln.getLineNumber() + " has: " + line);
 				tempArr = line.split(delimiter);
+				String classification = tempArr[1];
+				PlanetaryBodyReader.logger.debug("Classification has " + classification);
+
+				if (classification.contains("Dwarf")) {
+					PlanetaryBodyReader.logger.debug("Creating Dwarf planet object");
+
+					detail = new DwarfPlanet(
+							tempArr[0], 
+							tempArr[1], 
+							Double.parseDouble(tempArr[2]), 
+							Double.parseDouble(tempArr[3]), 
+							Double.parseDouble(tempArr[4]), 
+							Double.parseDouble(tempArr[5]), 
+							Double.parseDouble(tempArr[6]), 
+							Double.parseDouble(tempArr[7]), 
+							Double.parseDouble(tempArr[8]), 
+							Double.parseDouble(tempArr[9]));
+				}
 				
-				detail = new PlanetaryBody(
-						tempArr[0], 
-						tempArr[1], 
-						Double.parseDouble(tempArr[2]), 
-						Double.parseDouble(tempArr[3]), 
-						Double.parseDouble(tempArr[4]), 
-						Double.parseDouble(tempArr[5]), 
-						Double.parseDouble(tempArr[6]), 
-						Double.parseDouble(tempArr[7]), 
-						Double.parseDouble(tempArr[8]), 
-						Double.parseDouble(tempArr[9]));
+				else if (classification.contains("Planet")) {
+					PlanetaryBodyReader.logger.debug("Creating Planet object");
+
+					detail = new Planet(
+							tempArr[0], 
+							tempArr[1], 
+							Double.parseDouble(tempArr[2]), 
+							Double.parseDouble(tempArr[3]), 
+							Double.parseDouble(tempArr[4]), 
+							Double.parseDouble(tempArr[5]), 
+							Double.parseDouble(tempArr[6]), 
+							Double.parseDouble(tempArr[7]), 
+							Double.parseDouble(tempArr[8]), 
+							Double.parseDouble(tempArr[9]));
+				}
 				
-						list.add(detail);
-						}
+				else if (classification.contains("Moon")) {
+					PlanetaryBodyReader.logger.debug("Creating Moon planet object");
+
+					detail = new Moon(
+							tempArr[0], 
+							tempArr[1], 
+							Double.parseDouble(tempArr[2]), 
+							Double.parseDouble(tempArr[3]), 
+							Double.parseDouble(tempArr[4]), 
+							Double.parseDouble(tempArr[5]), 
+							Double.parseDouble(tempArr[6]), 
+							Double.parseDouble(tempArr[7]), 
+							Double.parseDouble(tempArr[8]), 
+							Double.parseDouble(tempArr[9]));
+				}
+				
+				PlanetaryBodyReader.logger.debug("Detail has " + detail);
+
+				list.add(detail);
+			}
 			
-			br.close();
-			
-		} catch (IOException ioe)
+			ln.close();
+		} catch (IOException ioe)		//ToDo : Fix exception here
 		{
 			ioe.printStackTrace();
 		} finally
 		{
-			if (null != br)
+			if (null != ln)
 			{
 				try
 				{
-					br.close();
+					ln.close();
 				} catch (IOException ioe)
 				{
 					ioe.printStackTrace();
@@ -82,9 +128,16 @@ public class PlanetaryBodyReader {
 		return list;
 	}	
 	
+	//Read the entries;
+		//input must be list (could this be something else?)
+		//for entry i, check "classification" attribute 
+		//if classification == air || land || sea
+			//assign proper child class
+	
+	//Classify the entries
+	
+	
 	public static void loadPlanetNamesArray() {
-		
-		
 	}
 	
 }
